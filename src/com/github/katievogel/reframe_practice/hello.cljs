1fb9ca11d5fb42927c648a4e705531000d30080a
Katie Vogel <kvog983@gmail.com>
2021-04-01 22:33:38 -0400

    core file set up

---
+++ src/com/github/katievogel/reframe_practice/hello.cljs
@@ -0,0 +1,7 @@
+(ns com.github.katievogel.reframe-practice.tabs_logic)
+
+;hello tab just static text
+;paragraphs tab add and remove paragraph -- info goes to app-db
+;log needs username and password entry and login button function -- info goes to app-db
+;fun people needs name, reason, add person button, populates card on dom, default value for no people
+;cards on fun people need some template text and removal button
